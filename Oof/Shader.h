7049568dbf01a339985ee47d746f41a20952ec7e
GachiKesha <stukalin.vlad@gmail.com>
2025-03-23 20:33:40 +0200

    refactor: split one-line declaration of different variables with same type

--- Oof/Shader.h
+++ Oof/Shader.h
@@ -4,7 +4,9 @@
 class Shader
 {
 public:
-	unsigned int shaderProgram, VAO, VBO;
+	unsigned int shaderProgram;
+	unsigned int VAO;
+	unsigned int VBO;
 
 	Shader(const char* vertexShaderPath, const char* fragmentShaderPath);
 private:
